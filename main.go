756ea7c4c16a320be101828e0711f326003a413a
dektlong <dekel.tankel@gmail.com>
2021-12-26 16:39:31 -0800

    Update main.go

--- main.go
+++ main.go
@@ -9,60 +9,43 @@ import (
 	"io/ioutil"
 )
 
-var IS_HAPPY = false
-
+var ALWAYS_HAPPY = false
 
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	
-	var override_backend = true
 	
 	log.Println(r.RemoteAddr, r.Method, r.URL.String())
 	
         fmt.Fprintf(w, "<H1><font color='navy'>Welcome to DevX on K8s demo</font></H1>")
 
-	fmt.Fprintf(w, "<H2><font color='maroon'>What is your mood today?</font>")
+	fmt.Fprintf(w, "<H2><font color='maroon'>What are your mood sensors reporting?</font>")
 	
 	fmt.Fprintf(w, "</font><BR><BR>")
 	
-	http.Get("http://devx-mood-backend.dekt-apps.serving.dekt.io/write")
+	if ALWAYS_HAPPY == false {
 	
-	http.Get("http://devx-mood-backend.dekt-apps.serving.dekt.io/write")
-
-	http.Get("http://devx-mood-backend.dekt-apps.serving.dekt.io/write")
+		response, err := http.Get("http://devx-mood-backend.dekt-apps.serving.dekt.io/sensors-data")
 	
-	response, err := http.Get("http://devx-mood-backend.dekt-apps.serving.dekt.io/sensors-data")
-	
-	if err != nil {
-		fmt.Fprintf(w,"ERROR! in calling API")
-	} else {
-		defer response.Body.Close()
-		responseData, err := ioutil.ReadAll(response.Body)
 		if err != nil {
-			fmt.Fprintf(w,"ERROR! in reading body")
+			fmt.Fprintf(w,"ERROR! in calling API")
 		} else {
-			fmt.Fprintf(w,string(responseData))
+			defer response.Body.Close()
+			responseData, err := ioutil.ReadAll(response.Body)
+			if err != nil {
+				fmt.Fprintf(w,"ERROR! in reading body")
+			} else {
+				fmt.Fprintf(w, "<font color='gray'>")
+				fmt.Fprintf(w,string(responseData))
+				fmt.Fprintf(w, "</font><BR><BR>")
+				fmt.Fprintf(w, "<font color='red'>")
+				fmt.Fprintf(w,"Your overall mood is not great. We hope you have a better day.")
+				fmt.Fprintf(w, "</font>")
+				fmt.Fprintf(w, "<BR><BR><img src='https://raw.githubusercontent.com/dektlong/devx-mood/main/sad-dog.jpg' alt=''>")
+			}
 		}
 	}
 	
-	if override_backend == false {
-	
-		response, err := http.Get("http://devx-mood-backend.dekt-apps.serving.dekt.io/sensors-data")
-
-		responseData, err := ioutil.ReadAll(response.Body)
-		
-		log.Println(err.Error())
-		
-		fmt.Fprintf(w,string(responseData))
-	}
-	
-	if IS_HAPPY == false {
-		fmt.Fprintf(w, "<font color='red'>")
-		fmt.Fprintf(w,"Your current mood is sad. We hope you have a better day.")
-		fmt.Fprintf(w, "</font>")
-		fmt.Fprintf(w, "<BR><BR><img src='https://raw.githubusercontent.com/dektlong/devx-mood/main/sad-dog.jpg' alt=''>")
-		
-    } else {
+    	} else {
 		fmt.Fprintf(w, "<font color='green'>")
 		fmt.Fprintf(w,"Your current mood is happy. Have an awsome rest of your day!")
 		fmt.Fprintf(w, "</font>")
