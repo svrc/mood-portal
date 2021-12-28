54b0513a2bb32ff9f6baae7fe399dadfc9d31081
dektlong <dekel.tankel@gmail.com>
2021-12-27 17:59:41 -0800

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"io/ioutil"
 )
 
-var ALWAYS_HAPPY = true
+var ALWAYS_HAPPY = false
 
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -19,29 +19,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "<H1><font color='navy'>Welcome to the DevX Mood Analyzer </font></H1><H2>")
 
 	if ALWAYS_HAPPY == false {
-	
-		fmt.Fprintf(w, "<font color='maroon'>Your mood sensors' current data:</font>")
-		fmt.Fprintf(w, "</font><BR><BR>")
-
-		response, err := http.Get("http://devx-mood-backend.dekt-apps.serving.dekt.io/sensors-data")
-	
-		if err != nil {
-			fmt.Fprintf(w,"ERROR! in calling API")
-		} else {
-			defer response.Body.Close()
-			responseData, err := ioutil.ReadAll(response.Body)
-			if err != nil {
-				fmt.Fprintf(w,"ERROR! in reading body")
-			} else {
-				fmt.Fprintf(w, "<font color='gray'>")
-				fmt.Fprintf(w,string(responseData))
-				fmt.Fprintf(w, "</font><BR><BR>")
-				fmt.Fprintf(w, "<font color='red'>")
-				fmt.Fprintf(w,"Your overall mood is not great. We hope it will get better.")
-				fmt.Fprintf(w, "</font>")
-				fmt.Fprintf(w, "<BR><BR><img src='https://raw.githubusercontent.com/dektlong/devx-mood/main/sad-dog.jpg' alt=''>")
-			}
-		}
+		fmt.Fprintf(w, "<font color='red'>")
+		fmt.Fprintf(w,"Your overall mood is not great. We hope it will get better.")
+		fmt.Fprintf(w, "</font>")
+		fmt.Fprintf(w, "<BR><BR><img src='https://raw.githubusercontent.com/dektlong/devx-mood/main/sad-dog.jpg' alt=''>")
 	} else {
 		fmt.Fprintf(w, "<font color='green'>")
 		fmt.Fprintf(w,"Your mood is always happy. Good for you!")
@@ -49,7 +30,21 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<BR><BR><img src='https://raw.githubusercontent.com/dektlong/devx-mood/main/happy-dog.jpg' alt=''>")
 	}
 	fmt.Fprintf(w, "</H2>")
-}
+	
+	//call backend-api and display sensor data
+	fmt.Fprintf(w, "<BR><BR>Mood sensors raw data:<BR>")
+	response, err := http.Get("http://devx-mood-backend.dekt-apps.serving.dekt.io/sensors-data")
+	if err != nil {
+		fmt.Fprintf(w,"ERROR! in calling API")
+	} else {
+		defer response.Body.Close()
+		responseData, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			fmt.Fprintf(w,"ERROR! in reading body")
+		} else {
+			fmt.Fprintf(w, "<font color='gray'>")
+			fmt.Fprintf(w,string(responseData))
+	}
 
 
 func main() {
