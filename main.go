45f2d7061f34e6fa1d29a6e64da5c93b8ce89d4d
dektlong <dekel.tankel@gmail.com>
2021-12-27 18:16:50 -0800

    Update main.go

--- main.go
+++ main.go
@@ -23,35 +23,42 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w,"Your overall mood is not great. We hope it will get better.")
 		fmt.Fprintf(w, "</font>")
 		fmt.Fprintf(w, "<BR><BR><img src='https://raw.githubusercontent.com/dektlong/devx-mood/main/sad-dog.jpg' alt=''>")
-	} else {
-		fmt.Fprintf(w, "<font color='green'>")
-		fmt.Fprintf(w,"Your mood is always happy. Good for you!")
-		fmt.Fprintf(w, "</font>")
-		fmt.Fprintf(w, "<BR><BR><img src='https://raw.githubusercontent.com/dektlong/devx-mood/main/happy-dog.jpg' alt=''>")
-	}
-	fmt.Fprintf(w, "</H2>")
-	
-	//call api to write sensor data backend-api and display sensor data
-	for i := 1; i < 6; i++ {
-    		http.Get("http://devx-mood-backend.dekt-apps.serving.dekt.io/write")
-	}
-	//call api to read sensor data and display it
-	fmt.Fprintf(w, "<BR><BR>Mood sensors raw data: ")
-	response, err := http.Get("http://devx-mood-backend.dekt-apps.serving.dekt.io/sensors-data")
-	if err != nil {
-		fmt.Fprintf(w,"ERROR! in calling API")
-	} else {
-		defer response.Body.Close()
-		responseData, err := ioutil.ReadAll(response.Body)
+		fmt.Fprintf(w, "</H2>")
+		
+		//call api to write sensor data backend-api and display sensor data
+		for i := 1; i < 6; i++ {
+    			http.Get("http://devx-mood-backend.dekt-apps.serving.dekt.io/write")
+		}
+		//call api to read sensor data and display it
+		fmt.Fprintf(w, "<BR><BR>Mood sensors raw data: ")
+		response, err := http.Get("http://devx-mood-backend.dekt-apps.serving.dekt.io/sensors-data")
 		if err != nil {
+			fmt.Fprintf(w,"ERROR! in calling API")
+		} else {
+			defer response.Body.Close()
+			responseData, err := ioutil.ReadAll(response.Body)
+			if err != nil {
 			fmt.Fprintf(w,"ERROR! in reading body")
 		} else {
 			fmt.Fprintf(w,string(responseData))
 		}	
 	}
+	} else {
+		fmt.Fprintf(w, "<font color='green'>")
+		fmt.Fprintf(w,"Your mood is always happy. Good for you!")
+		fmt.Fprintf(w, "</font>")
+		fmt.Fprintf(w, "<BR><BR><img src='https://raw.githubusercontent.com/dektlong/devx-mood/main/happy-dog.jpg' alt=''>")
+		fmt.Fprintf(w, "</H2>")
+	}
+	
 }
 
 
+func activateSensors(w http.ResponseWriter) {
+	
+	
+	
+}
 func main() {
 	
 	http.HandleFunc("/", handler)
@@ -60,3 +67,5 @@ func main() {
 	log.Printf("listening on %s", *addr)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
+
+
