892912b9bfdd3bc00a5d24c2e04739cfe81aadd8
dekt <dekel@vmware.com>
2022-04-07 14:55:11 -0500

    usually sad

--- main.go
+++ main.go
@@ -12,12 +12,12 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	
 	//bypass backend api call and always be happy
-	bypassBackend := true
+	bypassBackend := false
 
-	//sensorsWriteAPI := "http://mood-sensors.apps.dekt.io/activate"
-	sensorsWriteAPI := "http://localhost:8081/activate"
-	//sensorsReadAPI := "http://mood-sensors.apps.dekt.io/measure"
-	sensorsReadAPI := "http://localhost:8081/measure"
+	sensorsWriteAPI := "http://mood-sensors.apps.dekt.io/activate"
+	//sensorsWriteAPI := "http://localhost:8081/activate"
+	sensorsReadAPI := "http://mood-sensors.apps.dekt.io/measure"
+	//sensorsReadAPI := "http://localhost:8081/measure"
 	
 	log.Println(r.RemoteAddr, r.Method, r.URL.String())
 	
