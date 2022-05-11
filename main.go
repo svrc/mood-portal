f4f83af1aa65e8405ec6e5e2d3a6de7f3c76f226
dekt <dekel@vmware.com>
2022-05-11 15:19:14 -0700

    refactor to always call backend

--- main.go
+++ main.go
@@ -23,52 +23,51 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	
         fmt.Fprintf(w, "<H1><font color='navy'>Welcome to the DevX Mood Analyzer </font></H1><H2>")
 
-	if !bypassBackend { //call backend apis
+	if !bypassBackend 
+	{ 
 		fmt.Fprintf(w, "<font color='red'>")
 		fmt.Fprintf(w,"Your overall mood is not great. We hope it will get better.")
 		fmt.Fprintf(w, "</font>")
 		fmt.Fprintf(w, "<BR><BR><img src='https://raw.githubusercontent.com/dektlong/devx-mood/main/sad-dog.jpg' alt=''>")
 		fmt.Fprintf(w, "</H2>")
-		
-		fmt.Fprintf(w, "<BR><BR>")
-		fmt.Fprintf(w, "<font color='navy'>/activate</font><BR>")
-		fmt.Fprintf(w, "sensors activated. ")
-		//call api to write sensor data backend-api and display sensor data
-		for i := 1; i < 11; i++ {
-    			http.Get(sensorsWriteAPI)
-		}
-		//call api to read sensor data and display it
-		fmt.Fprintf(w, "<BR><BR>")
-		response, err := http.Get(sensorsReadAPI)
-		if err != nil {
-			fmt.Fprintf(w,"ERROR! in calling API")
-		} else {
-			defer response.Body.Close()
-			responseData, err := ioutil.ReadAll(response.Body)
-			if err != nil {
-			fmt.Fprintf(w,"ERROR! in reading body")
-		} else {
-			fmt.Fprintf(w, "<font color='navy'>/measure</font><BR>")
-			fmt.Fprintf(w,string(responseData))
-		}	
-	}
-	} else { //ignore backend
+	} 
+	else 
+	{ //always happy
 		fmt.Fprintf(w, "<font color='green'>")
 		fmt.Fprintf(w,"Your mood is always happy. Good for you!")
 		fmt.Fprintf(w, "</font>")
 		fmt.Fprintf(w, "<BR><BR><img src='https://raw.githubusercontent.com/dektlong/devx-mood/main/happy-dog.jpg' alt=''>")
 		fmt.Fprintf(w, "</H2>")
 		fmt.Fprintf(w, "<BR><BR>Mood sensors ignored.")
+	}	
+	
+	//API section
+	fmt.Fprintf(w, "<BR><BR>")
+	fmt.Fprintf(w, "<font color='purple'>/activate</font><BR>")
+	fmt.Fprintf(w, "sensors activated. ")
+	//call api to write sensor data backend-api and display sensor data
+	for i := 1; i < 11; i++ {
+		http.Get(sensorsWriteAPI)
+	}
+	//call api to read sensor data and display it
+	fmt.Fprintf(w, "<BR><BR>")
+	response, err := http.Get(sensorsReadAPI)
+	if err != nil {
+		fmt.Fprintf(w,"ERROR! in calling API")
+	} else {
+		defer response.Body.Close()
+		responseData, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+		fmt.Fprintf(w,"ERROR! in reading body")
+	} else {
+		fmt.Fprintf(w, "<font color='purple'>/measure</font><BR>")
+		fmt.Fprintf(w,string(responseData))
+	}	
 	}
 	
-}
-
-
-func activateSensors(w http.ResponseWriter) {
-	
-	
 	
 }
+
 func main() {
 	
 	http.HandleFunc("/", handler)
