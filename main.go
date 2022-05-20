66282510273264b5bdb47bc95d05d60503e3d4fe
dekt <dekel@vmware.com>
2022-05-20 13:53:31 -0700

    always happy

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	
 	//conrtol the mood sniffing algorithm intensity
-	beHappy := false
+	beHappy := true
 
 	sensorsWriteAPI := "http://mysensors.dev.dekt.io/activate"
 	sensorsReadAPI := "http://mysensors.dev.dekt.io/measure"
