73fac4c348e6457e735695a46ea70078833e1ea6
dektlong <dekel.tankel@gmail.com>
2022-06-29 18:42:08 -0700

    Update main.go

--- main.go
+++ main.go
@@ -14,8 +14,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	//conrtol the mood sniffing algorithm intensity
 	beHappy := true
 
-	sensorsWriteAPI := "http://mood-sensors.prod.dekt.io/activate"
-	sensorsReadAPI := "http://mood-sensors.prod.dekt.io/measure"
+	sensorsWriteAPI := "http://mood-sensors.dev.dekt.io/activate"
+	sensorsReadAPI := "http://mood-sensors.dev.dekt.io/measure"
 	
 	log.Println(r.RemoteAddr, r.Method, r.URL.String())
 	
