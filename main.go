2858cfeac6837c234ff1b8c0f05eab82ab7cd81f
dekt <dekel@vmware.com>
2022-08-08 02:10:07 -0700

    rename team to role

--- main.go
+++ main.go
@@ -96,14 +96,14 @@ func processSensorsMeasurement() (htmlOutput string) {
 	var allSensors AllSensors
 	json.Unmarshal(responseData, &allSensors.Sensors)
 
-	htmlOutput += "<table align='center'>"
+	htmlOutput += "<table>"
 	
-	htmlOutput += "<tr style='color:grey'>"
+	htmlOutput += "<tr style='color:grey' align='center'>"
 	htmlOutput += "<th>Sensor ID</th>" + "<th>Role</th>" + "<th>Mood</th></tr>"
 	htmlOutput += "</tr>"
 
 	for _, sensor := range allSensors.Sensors {
-  		htmlOutput += "<tr style='color:grey'>"
+  		htmlOutput += "<tr style='color:grey' align='center'>"
 		htmlOutput += "<td>" + strconv.Itoa(sensor.Id) + "</td>"
 		htmlOutput += "<td>" + sensor.Role + "&nbsp;&nbsp;&nbsp;&nbsp;</td>"
 		htmlOutput += "<td>" + sensor.Mood + "</td>"
