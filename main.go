22fceeaa97b3730f94c6552beaf028ab63384f5e
dekel <dekel@vmware.com>
2022-12-12 11:53:12 -0800

    usually sad

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ type AllSensors struct {
 func handler(w http.ResponseWriter, r *http.Request) {
 	
 	//conrtol the mood sniffing algorithm intensity
-	beHappy := true
+	beHappy := false
 
 	fmt.Fprintf(w, addHeader("DevX Mood Analyzer"))
     
